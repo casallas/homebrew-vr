7f91cd52b956948e3c86cd8ce84a38e992b2c65e
Juan Sebastian Casallas <casallas@iastate.edu>
2014-09-30 22:55:33 -0500

    vr-juggler: patch for named semaphores
    - There seems to be a problem with unnamed semaphores on Mac OS X, e.g. http://sourceforge.net/p/vrjuggler/mailman/message/10866972/

--- vr-juggler.rb
+++ vr-juggler.rb
@@ -15,6 +15,13 @@ class VrJuggler <Formula
   depends_on 'vrpn' => :recommended
   depends_on 'omniorb' => :optional
 
+  # There seems to be a problem with unnamed semaphores on Mac OS X
+  # e.g. http://sourceforge.net/p/vrjuggler/mailman/message/10866972/
+  patch do
+    url "https://gist.githubusercontent.com/anonymous/c4d725541a215b2267a1/raw/ae9c2c752a9493acfdbaa0c8caa4df440c9dfecf/vpr_semaphore_patch.diff"
+    sha1 "e3089919aa230d536d038d7658a172bb121fb659"
+  end
+
   def install
     args = ["..", "-DCMAKE_INSTALL_PREFIX='#{prefix}'", "-DCMAKE_BUILD_TYPE=None", "-DCMAKE_FIND_FRAMEWORK=LAST", "-Wno-dev", "-DBUILD_JAVA=OFF"]
     Dir.mkdir "build"
